d20b1a7b47268e471de7003ff37872b8905a7869
Haluk Eren Özlü <hlk.eren1907@gmail.com>
2024-03-20 12:00:53 +0300

    learnend how to work in the library

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
-game_list=["Rock","Paper","Scissors"]
+
+game_list = ['Rock', 'Paper', 'Scissors']
 computer = c = 0
 command = cmd = 0
-print("Score: Computer"+str(c)+"Player"+str(cmd))
+print("Score: Computer" + str(c) + "Player" + str(cmd))
+# the loop
+run = True
+while run:
+    computer_choice = random.choice(game_list)

786e869849a88aa0e27faa0f8ccf991c9fc50b5b
Haluk Eren Özlü <hlk.eren1907@gmail.com>
2024-03-21 03:46:33 +0300

    the basic functions of the game work without the pygame lib.

--- main.py
+++ main.py
@@ -14,10 +14,43 @@ command = cmd = 0
 print("Score: Computer" + str(c) + "Player" + str(cmd))
 
 # colors
-blue = (0, 0, 255)
+blue = (52, 70, 235)
+yellow = (237, 230, 24)
 
-# the loop
+# the game loop
 run = True
 while run:
     computer_choice = random.choice(game_list)
-
+    command = input("Rock, Paper, Scissors or Quit: ")
+    if command == computer_choice:
+        print("let's go")
+    elif command == 'Rock':
+        if computer_choice == 'Scissors':
+            print("Player won!")
+            cmd = cmd + 1
+        else:
+            print("Computer won")
+            c = c + 1
+    elif command == 'Paper':
+        if computer_choice == 'Rock':
+            print("Player won!")
+            cmd = cmd + 1
+        else:
+            print("Computer won")
+            c = c + 1
+    elif command == 'Scissors':
+        if computer_choice == 'Paper':
+            print("Player won!")
+            cmd = cmd + 1
+        else:
+            print("Computer won")
+            c = c + 1
+    elif command == 'Quit':
+        break
+    else:
+        print("Wrong command!")
+    print("Player: " + command)
+    print("Computer: " + computer_choice)
+    print("--------------")
+    print("Score: Computer: " + str(c) + "Player: " + str(cmd))
+    print("--------------")
